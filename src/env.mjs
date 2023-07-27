f83ddee13e4a52cec8091919d892b22ec85a5c78
Robert Starmer <rstarmer@gmail.com>
2023-07-26 19:34:03 -0700

    Update env.mjs to support change from Vercel to Supabase

--- src/env.mjs
+++ src/env.mjs
@@ -10,8 +10,7 @@ export const env = createEnv({
     TIER_BASE_URL: z.string().min(1),
     TIER_API_KEY: z.string().min(1),
     OPENAI_API_KEY: z.string().min(1),
-    POSTGRES_PRISMA_URL: z.string().min(1),
-    POSTGRES_URL_NON_POOLING: z.string().min(1),
+    DATABASE_URL: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
@@ -25,7 +24,6 @@ export const env = createEnv({
     TIER_BASE_URL: process.env.TIER_BASE_URL,
     TIER_API_KEY: process.env.TIER_API_KEY,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
-    POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
+    DATABASE_URL: process.env.POSTGRES_PRISMA_URL,
   },
 });
