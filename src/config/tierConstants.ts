5530e6e569858d75e7a9d3fc3e41ce56a1287fef
Robert Starmer <rstarmer@gmail.com>
2023-07-27 14:00:33 -0700

    Update tier plan targets
    src/config/tierConstants.ts had the wrong plan version
    pointers.  Updated to free@1, startup@2 and business@2

--- src/config/tierConstants.ts
+++ src/config/tierConstants.ts
@@ -1,9 +1,9 @@
 import { TierConstants } from "@/types";
 
 export const tierConstants: TierConstants = {
-  TIER_FREE_PLAN_ID: "plan:free@0",
-  TIER_STARTUP_PLAN_ID: "plan:startup@0",
-  TIER_BUSINESS_PLAN_ID: "plan:business@0",
+  TIER_FREE_PLAN_ID: "plan:free@1",
+  TIER_STARTUP_PLAN_ID: "plan:startup@2",
+  TIER_BUSINESS_PLAN_ID: "plan:business@2",
   TIER_BASE_FEATURE_ID: "feature:base",
   TIER_AICOPY_FEATURE_ID: "feature:aicopy",
   TIER_EXTRACOPY_FEATURE_ID: "feature:extraaicopy",
